bf0d916570b546784d4511c88697b345e0fb7006
N3oray <serge.lopes.94@free.fr>
2025-07-16 17:27:39 +0200

    ajout users-components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./users/users.module').then(m => m.UsersModule)
+  },
+  // Autres routes principales ici...
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: '**', redirectTo: 'users' }
+];
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
---
+++ frontend/src/app/users/users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  template: `
+    <div class="container my-4">
+      <h2>Liste des utilisateurs (lazy loaded)</h2>
+      <!-- Ajoute ici ton affichage dynamique -->
+    </div>
+  `
+})
+export class UsersComponent {}
