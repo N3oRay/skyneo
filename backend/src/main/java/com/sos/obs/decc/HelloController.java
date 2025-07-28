e160441c2152fe064b41e510f1cf2328561642b0
N3oray <serge.lopes.94@free.fr>
2025-07-28 02:59:14 +0200

    News version

---
+++ backend/src/main/java/com/sos/obs/decc/HelloController.java
@@ -0,0 +1,14 @@
+package com.sos.obs.decc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
