3c3af271d1a1cfc6c6f20d2cc828937c790f8910
N3oray <serge.lopes.94@free.fr>
2025-07-30 18:04:56 +0200

    adapation code

--- backend/src/main/java/com/sos/obs/decc/config/LiquibaseConfiguration.java
+++ backend/src/main/java/com/sos/obs/decc/config/LiquibaseConfiguration.java
@@ -1,47 +1,76 @@
-//package com.sos.obs.decc.config;
-//
-//import liquibase.integration.spring.SpringLiquibase;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.env.Environment;
-//import org.springframework.core.task.TaskExecutor;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class LiquibaseConfiguration {
-//
-//    private final Logger log = LoggerFactory.getLogger(LiquibaseConfiguration.class);
-//
-//    private final Environment env;
-//
-//
-//    public LiquibaseConfiguration(Environment env) {
-//        this.env = env;
-//    }
-//
-//    @Bean
-//    public SpringLiquibase liquibase(@Qualifier("taskExecutor") TaskExecutor taskExecutor,
-//            DataSource dataSource, LiquibaseProperties liquibaseProperties) {
-//
-//        // Use liquibase.integration.spring.SpringLiquibase if you don't want Liquibase to start asynchronously
-//        SpringLiquibase liquibase = new AsyncSpringLiquibase(taskExecutor, env);
-//        liquibase.setDataSource(dataSource);
-//        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
-//        liquibase.setContexts(liquibaseProperties.getContexts());
-//        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
-//        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
-//        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
-//        if (env.acceptsProfiles(Constants.SPRING_PROFILE_NO_LIQUIBASE)) {
-//            liquibase.setShouldRun(false);
-//        } else {
-//            liquibase.setShouldRun(liquibaseProperties.isEnabled());
-//            log.debug("Configuring Liquibase");
-//        }
-//        return liquibase;
-//    }
-//}
+package com.sos.obs.decc.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.task.TaskExecutor;
+import com.sos.obs.decc.config.AsyncSpringLiquibase;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(LiquibaseConfiguration.class);
+
+    private final Environment env;
+
+    public static final String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
+
+
+    public LiquibaseConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
+        liquibase.setDataSource(dataSource);
+        //liquibase.setDriverClassName(liquibaseProperties.getDataSource().getType()); // Test
+        //liquibase.setDataSource(liquibaseProperties.getDataSource());
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        //if (env.acceptsProfiles(Constants.SPRING_PROFILE_NO_LIQUIBASE)) {
+
+        if (env.acceptsProfiles(SPRING_PROFILE_NO_LIQUIBASE)) {
+            liquibase.setShouldRun(false);
+        } else {
+            liquibase.setShouldRun(liquibaseProperties.isEnabled());
+            log.debug("Configuring Liquibase");
+        }
+        return liquibase;
+    }
+
+    /*
+    @Bean
+    public SpringLiquibase liquibase(@Qualifier("taskExecutor") TaskExecutor taskExecutor,
+            DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+
+        // Use liquibase.integration.spring.SpringLiquibase if you don't want Liquibase to start asynchronously
+        SpringLiquibase liquibase = new AsyncSpringLiquibase(taskExecutor, env);
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
+        //liquibase.setDriverClassName(liquibaseProperties.getDriverClassName()); // important
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        //if (env.acceptsProfiles(Constants.SPRING_PROFILE_NO_LIQUIBASE)) {
+        if (env.acceptsProfiles(SPRING_PROFILE_NO_LIQUIBASE)) {
+            liquibase.setShouldRun(false);
+        } else {
+            liquibase.setShouldRun(liquibaseProperties.isEnabled());
+            log.debug("Configuring Liquibase");
+        }
+        return liquibase;
+    }
+    */
+}
